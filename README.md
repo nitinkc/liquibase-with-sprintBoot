746e5800fba04462dc83535ece393f430c3d700c
Nitin Chaurasia <nitin.chaurasia@davita.com>
2021-02-24 17:52:50 -0600

    Initial DDL Statements via Liquibase

---
+++ README.md
@@ -0,0 +1,1 @@
+# liquibase-with-sprintBoot
---
+++ src/main/resources/db/execute.sh
@@ -0,0 +1,8 @@
+liquibase \
+--driver=com.mysql.cj.jdbc.Driver \
+--classpath="/Users/nichaurasia/Downloads/mysql-connector-java-8.0.23/mysql-connector-java-8.0.23.jar" \
+--changeLogFile=db-changelog.xml \
+--url="jdbc:mysql://localhost:3306/liquibaseLearn" \
+--username=root \
+--password=nikloYahaSe!!2 \
+update
